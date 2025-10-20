0a92fb4100b2938af8af09841671eeff7cd219c4
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-19 19:34:22 -0700

    10 minute update

--- 210-midterm-2-starter-1.cpp
+++ 210-midterm-2-starter-1.cpp
@@ -214,7 +214,7 @@ public:
 
 	// This method traverses the doubly linked list starting at the head node, counts each node, and then returns the
 	// total number of nodes in the linked list.
-	int get_size_of_list() const{
+	int get_size_of_list() const {
 		Node *current = head;
 		int count = 0;
 		if (!current) {
@@ -227,81 +227,91 @@ public:
 		}
 		return count;
 	}
+};
 
 int main() {
-	// cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
+		// cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
 
-	// A vector is initialized and used to store the customer names read from the external data file
-	vector<string> customerNames;
-	ifstream infile;
-	infile.open("names.txt");
-	string temp;
-	if (infile.good()) {
-		while (infile >> temp) {
-			customerNames.push_back(temp);
-		}
-	} else
-		cout << "Error opening file" << endl;
-	infile.close();
+		// A vector is initialized and used to store the customer names read from the external data file
+		vector<string> customerNames;
+		ifstream infile;
+		infile.open("names.txt");
+		string temp;
+		if (infile.good()) {
+			while (infile >> temp) {
+				customerNames.push_back(temp);
+			}
+		} else
+			cout << "Error opening file" << endl;
+		infile.close();
 
-	// The DoublyLinkedList object is initialized and will be used to represent the coffee shop line
-	DoublyLinkedList line;
+		// The DoublyLinkedList object is initialized and will be used to represent the coffee shop line
+		DoublyLinkedList line;
 
-	cout << "Store opens:" << endl;
+		cout << "Store opens:" << endl;
 
-	// generate 5 random numbers to represent the index of the names in the vector
-	// add 5 nodes to the linked list with the indexes as the value
-	for (int i = 0; i < 5; i++) {
-		int index = rand() % customerNames.size();
-		line.push_back(index);
-		cout << '\t' << customerNames[index] << " joins the line" << endl;
-	}
+		// Backup solution: Add name indexes to this vector...
+		vector<int> initialNameIndexes(5);
 
-	// TODO: Fix
-	cout << "Resulting line:" << endl;
-	for (int i = 1; i <= 5; i++) {
-		int index = line.get_data_at_pos(i);
-		cout << "\t\t" << customerNames[index] << endl;
-	}
-
-	// starting i at 1 because the first time period (when the store opened and 5 customers were added) already happened
-	for (int i = 1; i < 20; i++) {
-		cout << "Time step #" << i + 1 << ':' << endl;
-		// A: 40% - The customer at the beginning of the line is being helped and ordering their coffee
-		int probability = rand() % 100 + 1; // should this be used for the whole loop or for each event?
-		if (probability <= 40) {
-			int index = line.pop_front();
-			cout << '\t' << customerNames[index] << " is served" << endl;
-		}
-		// B: 60% - A new customer joins the end of the line
-		if (probability <= 60) {
+		// generate 5 random numbers to represent the index of the names in the vector
+		// add 5 nodes to the linked list with the indexes as the value
+		for (int i = 0; i < 5; i++) {
 			int index = rand() % customerNames.size();
 			line.push_back(index);
-			cout << '\t' << customerNames[index] << " joined the line" << endl;
+			initialNameIndexes[i] = index;
+			cout << '\t' << customerNames[index] << " joins the line" << endl;
 		}
-		// C: 20% - The customer at the end of the line leaves
-		if (probability <= 20) {
-			int index = line.pop_back();
-			cout << '\t' << customerNames[index] << " (at the rear) left the line" << endl;
+
+		cout << "\t Resulting line:" << endl;
+		// TODO: Fix
+		// for (int i = 1; i <= 5; i++) {
+		// 	int index = line.get_data_at_pos(i);
+		// }
+
+		// Backup solution: Use the initialNameIndexes vector to output initial list of names
+		for (int index: initialNameIndexes) {
+			cout << "\t\t" << customerNames[index] << endl;
 		}
-		// D: 10% - Any customer leaves
-		if (probability <= 10) {
-			int size = line.get_size_of_list();
-			int randomNode = rand() % size + 1; // delete_pos() has 1 as the position of the head node
-			int index = line.delete_pos(randomNode);
-			cout << '\t' << customerNames[index] << " left the line" << endl;
+
+		// starting i at 1 because the first time period (when the store opened and 5 customers were added) already happened
+		for (int i = 1; i < 20; i++) {
+			cout << "Time step #" << i + 1 << ':' << endl;
+			// A: 40% - The customer at the beginning of the line is being helped and ordering their coffee
+			int probability = rand() % 100 + 1; // should this be used for the whole loop or for each event?
+			if (probability <= 40) {
+				int index = line.pop_front();
+				cout << '\t' << customerNames[index] << " is served" << endl;
+			}
+			// B: 60% - A new customer joins the end of the line
+			if (probability <= 60) {
+				int index = rand() % customerNames.size();
+				line.push_back(index);
+				cout << '\t' << customerNames[index] << " joined the line" << endl;
+			}
+			// C: 20% - The customer at the end of the line leaves
+			if (probability <= 20) {
+				int index = line.pop_back();
+				cout << '\t' << customerNames[index] << " (at the rear) left the line" << endl;
+			}
+			// D: 10% - Any customer leaves
+			if (probability <= 10) {
+				int size = line.get_size_of_list();
+				int randomNode = rand() % size + 1; // delete_pos() has 1 as the position of the head node
+				int index = line.delete_pos(randomNode);
+				cout << '\t' << customerNames[index] << " left the line" << endl;
+			}
+			// E: 10% - A VIP customer skips the line, goes straight to the counter, and orders
+			if (probability <= 10) {
+				int index = rand() % customerNames.size();
+				line.push_back(index);
+				cout << '\t' << customerNames[index] << " (VIP) joins the line" << endl;
+				// Unclear if the customer served right away and thus removed from the line
+				line.pop_front();
+				cout << '\t' << customerNames[index] << " (VIP) is served" << endl;
+			}
 		}
-		// E: 10% - A VIP customer skips the line, goes straight to the counter, and orders
-		if (probability <= 10) {
-			int index = rand() % customerNames.size();
-			line.push_back(index);
-			cout << '\t' << customerNames[index] << " (VIP) joins the line" << endl;
-			// Unclear if the customer served right away and thus removed from the line
-			line.pop_front();
-			cout << '\t' << customerNames[index] << " (VIP) is served" << endl;
-		}
+
+
+		return 0;
 	}
 
-
-	return 0;
-}
