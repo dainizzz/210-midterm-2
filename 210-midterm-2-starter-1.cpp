64b84c5e97341bd21787f887155a334c9141684b
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-19 18:25:04 -0700

    10 minute update

--- 210-midterm-2-starter-1.cpp
+++ 210-midterm-2-starter-1.cpp
@@ -7,253 +7,276 @@ const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 
 class DoublyLinkedList {
 private:
-    struct Node {
-        int data;
-        Node* prev;
-        Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
-            data = val; 
-            prev = p;
-            next = n;
-        }
-    };
+	struct Node {
+		int data;
+		Node *prev;
+		Node *next;
 
-    Node* head;
-    Node* tail;
+		Node(int val, Node *p = nullptr, Node *n = nullptr) {
+			data = val;
+			prev = p;
+			next = n;
+		}
+	};
+
+	Node *head;
+	Node *tail;
 
 public:
-    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+	DoublyLinkedList() {
+		head = nullptr;
+		tail = nullptr;
+	}
 
-    void insert_after(int value, int position) {
-        if (position < 0) {
-            cout << "Position must be >= 0." << endl;
-            return;
-        }
+	void insert_after(int value, int position) {
+		if (position < 0) {
+			cout << "Position must be >= 0." << endl;
+			return;
+		}
 
-        Node* newNode = new Node(value);
-        if (!head) {
-            head = tail = newNode;
-            return;
-        }
+		Node *newNode = new Node(value);
+		if (!head) {
+			head = tail = newNode;
+			return;
+		}
 
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i)
-            temp = temp->next;
+		Node *temp = head;
+		for (int i = 0; i < position && temp; ++i)
+			temp = temp->next;
 
-        if (!temp) {
-            cout << "Position exceeds list size. Node not inserted.\n";
-            delete newNode;
-            return;
-        }
+		if (!temp) {
+			cout << "Position exceeds list size. Node not inserted.\n";
+			delete newNode;
+			return;
+		}
 
-        newNode->next = temp->next;
-        newNode->prev = temp;
-        if (temp->next)
-            temp->next->prev = newNode;
-        else
-            tail = newNode;
-        temp->next = newNode;
-    }
+		newNode->next = temp->next;
+		newNode->prev = temp;
+		if (temp->next)
+			temp->next->prev = newNode;
+		else
+			tail = newNode;
+		temp->next = newNode;
+	}
 
-    void delete_val(int value) {
-        if (!head) return;
+	void delete_val(int value) {
+		if (!head) return;
 
-        Node* temp = head;
-        
-        while (temp && temp->data != value)
-            temp = temp->next;
+		Node *temp = head;
 
-        if (!temp) return; 
+		while (temp && temp->data != value)
+			temp = temp->next;
 
-        if (temp->prev)
-            temp->prev->next = temp->next;
-        else
-            head = temp->next; 
+		if (!temp) return;
 
-        if (temp->next)
-            temp->next->prev = temp->prev;
-        else
-            tail = temp->prev; 
+		if (temp->prev)
+			temp->prev->next = temp->next;
+		else
+			head = temp->next;
 
-        delete temp;
-    }
+		if (temp->next)
+			temp->next->prev = temp->prev;
+		else
+			tail = temp->prev;
 
-    void delete_pos(int pos) {
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
-        }
-    
-        if (pos == 1) {
-            pop_front();
-            return;
-        }
-    
-        Node* temp = head;
-    
-        for (int i = 1; i < pos; i++){
-            if (!temp) {
-                cout << "Position doesn't exist." << endl;
-                return;
-            }
-            else
-                temp = temp->next;
-        }
-        if (!temp) {
-            cout << "Position doesn't exist." << endl;
-            return;
-        }
-    
-        if (!temp->next) {
-            pop_back();
-            return;
-        }
-    
-        Node* tempPrev = temp->prev;
-        tempPrev->next = temp->next;
-        temp->next->prev = tempPrev;
-        delete temp;
-    }
+		delete temp;
+	}
 
-    void push_back(int v) {
-        Node* newNode = new Node(v);
-        if (!tail)
-            head = tail = newNode;
-        else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-    }
-    
-    void push_front(int v) {
-        Node* newNode = new Node(v);
-        if (!head)
-            head = tail = newNode;
-        else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
-    }
-    
-    void pop_front() {
+	void delete_pos(int pos) {
+		if (!head) {
+			cout << "List is empty." << endl;
+			return;
+		}
 
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
-        }
+		if (pos == 1) {
+			pop_front();
+			return;
+		}
 
-        Node * temp = head;
+		Node *temp = head;
 
-        if (head->next) {
-            head = head->next;
-            head->prev = nullptr;
-        }
-        else
-            head = tail = nullptr;
-        delete temp;
-    }
+		for (int i = 1; i < pos; i++) {
+			if (!temp) {
+				cout << "Position doesn't exist." << endl;
+				return;
+			} else
+				temp = temp->next;
+		}
+		if (!temp) {
+			cout << "Position doesn't exist." << endl;
+			return;
+		}
 
-    void pop_back() {
-        if (!tail) {
-            cout << "List is empty." << endl;
-            return;
-        }
-        Node * temp = tail;
+		if (!temp->next) {
+			pop_back();
+			return;
+		}
 
-        if (tail->prev) {
-            tail = tail->prev;
-            tail->next = nullptr;
-        }
-        else
-            head = tail = nullptr;
-        delete temp;
-    }
+		Node *tempPrev = temp->prev;
+		tempPrev->next = temp->next;
+		temp->next->prev = tempPrev;
+		delete temp;
+	}
 
-    ~DoublyLinkedList() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
-    void print() {
-        Node* current = head;
-        if (!current) {
-            cout << "List is empty." << endl;
-            return;
-        }
-        while (current) {
-            cout << current->data << " ";
-            current = current->next;
-        }
-        cout << endl;
-    }
+	void push_back(int v) {
+		Node *newNode = new Node(v);
+		if (!tail)
+			head = tail = newNode;
+		else {
+			tail->next = newNode;
+			newNode->prev = tail;
+			tail = newNode;
+		}
+	}
 
-    void print_reverse() {
-        Node* current = tail;
-        if (!current) { 
-            cout << "List is empty." << endl;
-            return;
-        }
-        while (current) {
-            cout << current->data << " ";
-            current = current->prev;
-        }
-        cout << endl;
-    }
+	void push_front(int v) {
+		Node *newNode = new Node(v);
+		if (!head)
+			head = tail = newNode;
+		else {
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;
+		}
+	}
+
+	void pop_front() {
+		if (!head) {
+			cout << "List is empty." << endl;
+			return;
+		}
+
+		Node *temp = head;
+
+		if (head->next) {
+			head = head->next;
+			head->prev = nullptr;
+		} else
+			head = tail = nullptr;
+		delete temp;
+	}
+
+	void pop_back() {
+		if (!tail) {
+			cout << "List is empty." << endl;
+			return;
+		}
+		Node *temp = tail;
+
+		if (tail->prev) {
+			tail = tail->prev;
+			tail->next = nullptr;
+		} else
+			head = tail = nullptr;
+		delete temp;
+	}
+
+	~DoublyLinkedList() {
+		while (head) {
+			Node *temp = head;
+			head = head->next;
+			delete temp;
+		}
+	}
+
+	void print() {
+		Node *current = head;
+		if (!current) {
+			cout << "List is empty." << endl;
+			return;
+		}
+		while (current) {
+			cout << current->data << " ";
+			current = current->next;
+		}
+		cout << endl;
+	}
+
+	void print_reverse() {
+		Node *current = tail;
+		if (!current) {
+			cout << "List is empty." << endl;
+			return;
+		}
+		while (current) {
+			cout << current->data << " ";
+			current = current->prev;
+		}
+		cout << endl;
+	}
 };
 
 int main() {
-    // cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
+	// cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
 
-    // Simulate a line at a coffeshop, running for 20 minutes (time periods) and displaying the current line
-    // When the store opens (first time period), add 5 customers to the line right away
-    // Every time period after that, multiple events will occur with a given probability:
-    //     40% - The customer at the beginning of the line is being helped and ordering their coffee
-    //     60% - A new customer joins the end of the line
-    //     20% - The customer at the end of the line leaves
-    //     10% - Any customer leaves
-    //     10% - A VIP customer skips the line, goes straight to the counter, and orders
-    // names.txt has the customer names
-    // Display each event that occurs
+	// Simulate a line at a coffeshop, running for 20 minutes (time periods) and displaying the current line
+	// When the store opens (first time period), add 5 customers to the line right away
+	// Every time period after that, multiple events will occur with a given probability:
+	//     40% - The customer at the beginning of the line is being helped and ordering their coffee
+	//     60% - A new customer joins the end of the line
+	//     20% - The customer at the end of the line leaves
+	//     10% - Any customer leaves
+	//     10% - A VIP customer skips the line, goes straight to the counter, and orders
+	// names.txt has the customer names
+	// Display each event that occurs
 
-    // TODO
-    // create a vector and add the names from the text file to the vector
-    vector<string> customerNames;
-    ifstream infile;
-    infile.open("names.txt");
-    string temp;
-    if (infile.good()) {
-        while (infile >> temp) {
-            customerNames.push_back(temp);
-        }
-    } else
-        cout << "Error opening file" << endl;
-    infile.close();
+	// TODO
+	// create a vector and add the names from the text file to the vector
+	vector<string> customerNames;
+	ifstream infile;
+	infile.open("names.txt");
+	string temp;
+	if (infile.good()) {
+		while (infile >> temp) {
+			customerNames.push_back(temp);
+		}
+	} else
+		cout << "Error opening file" << endl;
+	infile.close();
 
-    // create a linked list to represent the line
-    DoublyLinkedList line;
+	// create a linked list to represent the line
+	DoublyLinkedList line;
 
-    // Output
-    cout << "Store opens:" << endl;
+	// Output
+	cout << "Store opens:" << endl;
 
-    // generate 5 random numbers to represent the index of the names in the vector
-    // add 5 nodes to the linked list with the indeces as the value
-    for (int i = 0; i < 5; i++) {
-        int index = rand() % customerNames.size();
-        line.push_back(index);
-        cout << '\t' << customerNames[index] << " joins the line" << endl;
-    }
+	// generate 5 random numbers to represent the index of the names in the vector
+	// add 5 nodes to the linked list with the indeces as the value
+	for (int i = 0; i < 5; i++) {
+		int index = rand() % customerNames.size();
+		line.push_back(index);
+		cout << '\t' << customerNames[index] << " joins the line" << endl;
+	}
 
-    cout << "Resulting line:" << endl;
-    line.print(); // Need to add method to get the values
+	cout << "Resulting line:" << endl;
+	line.print(); // Need to add method to get the values
+	int probability;
+	// starting i at 1 because the first time period (when the store opened and 5 customers were added) already happened
+	for (int i = 1; i < 20; i++) {
+		// A: 40% - The customer at the beginning of the line is being helped and ordering their coffee
+		probability = rand() % 100 % 1; // should this be used for the whole loop or for each event?
+		if (probability <= 40) {
+			line.pop_front();
+		}
+		// B: 60% - A new customer joins the end of the line
+		if (probability <= 60) {
+			int index = rand() % customerNames.size();
+			line.push_back(index);
+			cout << '\t' << customerNames[index] << " joins the line" << endl;
+		}
+		// C: 20% - The customer at the end of the line leaves
+		if (probability <= 20) {
+			line.pop_back();
+		}
+		// D: 10% - Any customer leaves
+		if (probability <= 10) {
+			// get size of linked list and pop a random node
+		}
+		// E: 10% - A VIP customer skips the line, goes straight to the counter, and orders
+		if (probability <= 10) {
+		}
+	}
 
-    for (int i = 0; i < 19; i++) {
 
-    }
-
-    
-    return 0;
+	return 0;
 }
